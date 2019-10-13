b8ea999e5ed634d60272657a5a77ae947e59871b
Charles Yang <yc893515385@gmail.com>
2019-10-13 17:05:32 +0800

    Update archlinux.sh

--- sync/archlinux.sh
+++ sync/archlinux.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-sync_path="/mirrors"
-sync_url="rsync://rsync.mirrors.ustc.edu.cn/repo"
-sync_name="archlinux"
+export sync_path="/mirrors"
+export sync_url="rsync://rsync.mirrors.ustc.edu.cn/repo"
+export sync_name="archlinux"
 
-touch $sync_path/status/$sync_name
-
-if [[ `cat "$sync_path/status/$sync_name"` != "syncing" ]];
-then
-        echo "[$sync_name] Syncing..."
-        echo "syncing" > "../$sync_name"
-        rsync -rltz4 --progress --delete --log-file="$sync_path/log/$sync_name.log" "$sync_url/$sync_name" "$sync_path/data/$sync_name"
-        if [ $? -ne 0 ] ;
-        then
-                echo "[$sync_name] Sync failed.";
-                echo "failed" > "$sync_path/status/$sync_name";
-        else
-                echo "[$sync_name] Sync completed.";
-                echo "normal" > "$sync_path/status/$sync_name";
-        fi
-else
-        echo "[$sync_name] Disabled, skipping..."
-fi
+sh "$sync_path/sync/rsync_common.sh"
