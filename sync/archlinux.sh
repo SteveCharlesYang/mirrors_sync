fcef91d9d676e08539c4fbcccab624bc095f0198
12wk34 <12wk34@gmail.com>
2019-10-13 16:59:56 +0800

    use rsync_common

--- sync/archlinux.sh
+++ sync/archlinux.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-sync_path="/mirrors"
-sync_url="rsync://rsync.mirrors.ustc.edu.cn/repo"
-sync_name="archlinux"
+export sync_path="/mirrors"
+export sync_url="rsync://rsync.mirrors.ustc.edu.cn/repo"
+export sync_name="archlinux"
 
-if [ `cat "$sync_path/status/$sync_name"` = "normal" ] ; then
-	echo "syncing" > "$sync_path/status/$sync_name"
-	rsync -rltz4 --progress --delete --log-file="$sync_path/log/$sync_name.log" "$sync_url/$sync_name" "$sync_path/data/$sync_name"
-	echo "normal" > "$sync_path/status/$sync_name"
-fi
+sh "$sync_path/sync/rsync_common.sh"
--- sync/rsync_common.sh
+++ sync/rsync_common.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sync_path="/mirrors"
-sync_url="rsync://mirrors.tuna.tsinghua.edu.cn"
-sync_name="ctex"
-
 if [ `cat "$sync_path/status/$sync_name"` = "normal" ] ; then
 	echo "syncing" > "$sync_path/status/$sync_name"
 	rsync -rltz4 --progress --delete --log-file="$sync_path/log/$sync_name.log" "$sync_url/$sync_name" "$sync_path/data/$sync_name"
