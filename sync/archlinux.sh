122c7ad89354a9143348930fe013cc50c0adc5aa
12wk34 <12wk34@gmail.com>
2019-10-13 16:16:06 +0800

    fix path

--- sync/archlinux.sh
+++ sync/archlinux.sh
@@ -6,6 +6,6 @@ sync_name="archlinux"
 
 if [ `cat "$sync_path/status/$sync_name"` = "normal" ] ; then
 	echo "syncing" > "$sync_path/status/$sync_name"
-	rsync -rltz4 --progress --delete --log-file="$sync_path/log/archlinux.log" "$sync_url/$sync_name" "$sync_path/data/$sync_name"
+	rsync -rltz4 --progress --delete --log-file="$sync_path/log/$sync_name.log" "$sync_url/$sync_name" "$sync_path/data/$sync_name"
 	echo "normal" > "$sync_path/status/$sync_name"
 fi
